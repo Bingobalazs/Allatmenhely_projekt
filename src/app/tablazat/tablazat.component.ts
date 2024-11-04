093ff604afce61949b1355daed6cec05656e5126
MonokiGabor2005 <monokigabor2005@gmail.com>
2024-11-04 11:14:41 +0100

    ts módosítás

--- src/app/tablazat/tablazat.component.ts
+++ src/app/tablazat/tablazat.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-tablazat',
   standalone: true,
-  imports: [],
+  imports: [ApiModule],
   templateUrl: './tablazat.component.html',
   styleUrl: './tablazat.component.css'
 })
 export class TablazatComponent {
+ allatok: any[]=[]; // Tárolja az API-tól kapott adatokat
+ 
+ export class DataService {
+  private apiUrl = 'https://balgalazs.moriczcloud.hu/allat/mind'; // Az API URL-je
+
+ constructor(private dataService: TablazatComponent) { }
+
+ ngOnInit(): void {
+   // Az API adatainak lekérése a komponens inicializálásakor
+   this.dataService.getAllatok().subscribe(
+     (data) => {
+       this.allatok = data;
+     },
+     (error) => {
+       console.error('Hiba az adatok lekérésekor:', error);
+     }
+   );
+ }
+
+  getAllatok(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
 
 }
