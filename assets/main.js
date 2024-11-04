13e2f433e9f0885dd4c06824a61bfe26089a23fb
KCsaba1 <113901591+KCsaba1@users.noreply.github.com>
2024-11-04 10:04:59 +0100

    a gyasz

---
+++ assets/main.js
@@ -0,0 +1,52 @@
+//Ez egy js kód, egy jobb világ számára. Remélem eyszer olvassa valaki
+
+
+function Oltas(o)
+{
+    oltasok = {
+        oltas1: 0,
+        oltas2: 0,
+        oltas3: 0,
+    }
+    var oltas = document.getElementById('oltas');
+    var box = document.getElementById('box');
+    box.innerHTML = "";
+
+    if (o = "o1")
+    {
+        if (oltasok.oltas1 == 0)
+        {
+            oltasok.oltas1 = 1;
+        }
+        else
+        {
+            oltasok.oltas1 = 0;
+        }
+    }
+    else if (o = "o2")
+    {
+        if (oltasok.oltas2 == 0)
+        {
+            oltasok.oltas2 = 1;
+        }
+        else
+        {
+            oltasok.oltas2 = 0;
+        }
+    }
+    else if (o = "o3")
+    {
+        if (oltasok.oltas3 == 0)
+        {
+            oltasok.oltas3 = 1;
+        }
+        else
+        {
+            oltasok.oltas3 = 0;
+        }
+    }
+
+    if (oltasok.oltas1 = 1) {box.innerHTML += "oltas1"}
+    else if (oltasok.oltas2 = 1) {box.innerHTML += "oltas2"}
+    else if (oltasok.oltas3 = 1) {box.innerHTML += "oltas3"}
+}
